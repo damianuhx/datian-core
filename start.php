59dc7aa5eff1d7f38a0cc6f7635ca4cde8d97764
damianh82 <d.hiltebrand@examprep.ch>
2024-07-19 12:34:14 +0200

    added includes to start.php

--- start.php
+++ start.php
@@ -8,7 +8,16 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
 
-include 'includes.php';
+include_once 'env.php';
+
+//require 'vendor/autoload.php';
+foreach (glob("settings/*.php") as $filename)
+{
+    include_once $filename;
+}
+
+include_once 'datian-core/helper.php';
+include_once 'datian-core/crud.php';
 
 //make path an array
 $return = ['message'=>'', 'warning'=>'', 'debug'=>[], 'data'=>[], 'log'=>[] ];
